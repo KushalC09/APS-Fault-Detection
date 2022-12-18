016cbc39e0602f610ed2e92b030faa07e858ca83
KushalC09 <kchaple9@gmail.com>
2022-12-18 18:15:03 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="sensors",
+    version = "0.0.1",
+    author = "ineuron",
+    author_email = "kchaple9@gmial.com",
+    packages = find_packages(),
+    install_requires = get_requirenments(),
+)
